36b7eeb05a6385930e552f459dccbd06c800a564
Jeroen Groenveld <jeroengroenveld@live.nl>
2019-02-11 15:08:16 +0100

    Cache support (#48)
    * Added missing 'createCacheStore' function
    
    * Added  league/flysystem-cached-adapter as composer requirement
    
    * Added missing classes

--- src/GoogleCloudStorageServiceProvider.php
+++ src/GoogleCloudStorageServiceProvider.php
@@ -2,13 +2,15 @@
 
 namespace Superbalist\LaravelGoogleCloudStorage;
 
-use Google\Cloud\Storage\StorageClient;
-use Illuminate\Filesystem\FilesystemManager;
 use Illuminate\Support\Arr;
-use Illuminate\Support\ServiceProvider;
-use League\Flysystem\AdapterInterface;
-use League\Flysystem\Cached\CachedAdapter;
+use Illuminate\Filesystem\Cache;
 use League\Flysystem\Filesystem;
+use League\Flysystem\AdapterInterface;
+use Illuminate\Support\ServiceProvider;
+use Google\Cloud\Storage\StorageClient;
+use League\Flysystem\Cached\CachedAdapter;
+use Illuminate\Filesystem\FilesystemManager;
+use League\Flysystem\Cached\Storage\Memory as MemoryStore;
 use Superbalist\Flysystem\GoogleStorage\GoogleStorageAdapter;
 
 class GoogleCloudStorageServiceProvider extends ServiceProvider
@@ -16,8 +18,8 @@ class GoogleCloudStorageServiceProvider extends ServiceProvider
     /**
      * Create a Filesystem instance with the given adapter.
      *
-     * @param  \League\Flysystem\AdapterInterface  $adapter
-     * @param  array  $config
+     * @param  \League\Flysystem\AdapterInterface $adapter
+     * @param  array $config
      * @return \League\Flysystem\FlysystemInterfaceAdapterInterface
      */
     protected function createFilesystem(AdapterInterface $adapter, array $config)
@@ -34,11 +36,33 @@ class GoogleCloudStorageServiceProvider extends ServiceProvider
     }
 
     /**
+     * Create a cache store instance.
+     *
+     * @param  mixed $config
+     * @return \League\Flysystem\Cached\CacheInterface
+     *
+     * @throws \InvalidArgumentException
+     */
+    protected function createCacheStore($config)
+    {
+        if ($config === true) {
+            return new MemoryStore;
+        }
+
+        return new Cache(
+            $this->app['cache']->store($config['store']),
+            $config['prefix'] ?? 'flysystem',
+            $config['expire'] ?? null
+        );
+    }
+
+    /**
      * Perform post-registration booting of services.
      */
     public function boot()
     {
-        $factory = $this->app->make('filesystem'); /* @var FilesystemManager $factory */
+        $factory = $this->app->make('filesystem');
+        /* @var FilesystemManager $factory */
         $factory->extend('gcs', function ($app, $config) {
             $storageClient = new StorageClient([
                 'projectId' => $config['project_id'],
